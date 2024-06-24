d394083377792f2b3cc311102f35766173700bf2
OVERWRITTE <getoverwrite@gmail.com>
2024-06-24 12:10:55 -0600

    Contrast update

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,11 @@ function App() {
       </div>
       <h1>Vite + React + TS</h1>
       <div>
-        <button className=' text-black' onClick={sendNotification}>
+        <button className=' text-black bg-white' onClick={sendNotification}>
           Send Notification
         </button>
         <p className=" text-zinc-400 p-2">
-          Presseted by Marco C.
+          Preseted by Marco C.
         </p>
       </div>
       <p>
